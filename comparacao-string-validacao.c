298bba5f2376dc43f1ad7773a3e8f8082e928aee
Lauan Victor <52393559+LauanV@users.noreply.github.com>
2023-10-31 19:32:38 -0300

    comparacao-string-validacao.c

--- comparacao-string-validacao.c
+++ comparacao-string-validacao.c
@@ -3,15 +3,15 @@
 
 int main(){
 
-    char senha [20];
-    char validacao [20] = {"senha"};
+    char senha [20]; // Var para determinar onde guardar a "senha" e o tamanho máximo da senha.
+    char validacao [20] = {"senha"}; // Var que determina a "senha" correta (entre os '{}').
 
     printf("Informe sua senha:\n");
     gets(senha);
 
-    int comparacao = strcmp(senha, validacao);
+    int comparacao = strcmp(senha, validacao); // Comparação das duas strings para saber se são iguais.
 
-    if(comparacao == 0){
+    if(comparacao == 0){ // Se a comparação feita anteriormente retornar o valor '0' significa que as strings são iguais.
 
         printf("Validado.");
 
